8113e017af40a1bf014234a30670bf7cb7c198da
Jason Kunisaki <jason.kunisaki@hsc.utah.edu>
2021-05-11 15:17:48 -0600

    multiprocessing tutorial

---
+++ python/multiprocessing_tutorial.py
@@ -0,0 +1,130 @@
+#%%
+"""
+Multiprocessing the old fashioned way
+"""
+import multiprocessing
+import time
+
+start = time.perf_counter()
+
+def do_something(seconds):
+    print(f'Sleeping for {seconds} second(s)...')
+    time.sleep(1)
+    print('Done sleeping...')
+
+processes = []
+
+for _ in range(10):
+    p = multiprocessing.Process(target=do_something, args=[1.5])
+    p.start()
+    processes.append(p)
+
+for process in processes:
+    process.join()    
+    
+finish = time.perf_counter()
+
+print(f'Finished in {round(finish-start, 2)} seconds(s)')
+
+#%%
+"""
+multiprocessing with process pool executor --> best to use with context manager
+"""
+import concurrent.futures
+import time
+
+start = time.perf_counter()
+
+def do_something(seconds):
+    print(f'Sleeping for {seconds} second(s)...')
+    time.sleep(seconds)
+    return f'Done sleeping...{seconds}'
+    
+with concurrent.futures.ProcessPoolExecutor() as executor:
+    secs = [5,4,3,2,1]
+    results = [executor.submit(do_something, sec) for sec in secs] ## Using list comprehension
+    
+    for f in concurrent.futures.as_completed(results):
+        print(f.result())
+    
+    # f1 = executor.submit(do_something, 1) ## submit schedules a function to be run --> returns a future object (which stores output)
+    # print(f1.result())
+
+finish = time.perf_counter()
+
+print(f'Finished in {round(finish-start, 2)} seconds(s)')
+
+#%%
+"""
+Multiprocessing with built in 'map' method
+"""
+import concurrent.futures
+import time
+
+start = time.perf_counter()
+
+def do_something(seconds):
+    print(f'Sleeping for {seconds} second(s)...')
+    time.sleep(seconds)
+    return f'Done sleeping...{seconds}'
+    
+with concurrent.futures.ProcessPoolExecutor() as executor:
+    secs = [5,4,3,2,1]
+    results = executor.map(do_something, secs) ## Map returns results in the order they were started
+    
+    for result in results:
+        print(result)
+
+finish = time.perf_counter()
+
+print(f'Finished in {round(finish-start, 2)} seconds(s)')
+
+#%%
+"""
+Real world example
+"""
+
+import time
+import concurrent.futures
+from PIL import Image, ImageFilter
+
+img_names = [
+    'photo-1516117172878-fd2c41f4a759.jpg',
+    'photo-1532009324734-20a7a5813719.jpg',
+    'photo-1524429656589-6633a470097c.jpg',
+    'photo-1530224264768-7ff8c1789d79.jpg',
+    'photo-1564135624576-c5c88640f235.jpg',
+    'photo-1541698444083-023c97d3f4b6.jpg',
+    'photo-1522364723953-452d3431c267.jpg',
+    'photo-1513938709626-033611b8cc03.jpg',
+    'photo-1507143550189-fed454f93097.jpg',
+    'photo-1493976040374-85c8e12f0c0e.jpg',
+    'photo-1504198453319-5ce911bafcde.jpg',
+    'photo-1530122037265-a5f1f91d3b99.jpg',
+    'photo-1516972810927-80185027ca84.jpg',
+    'photo-1550439062-609e1531270e.jpg',
+    'photo-1549692520-acc6669e2f0c.jpg'
+]
+
+t1 = time.perf_counter()
+
+size = (1200, 1200) ## Resize photos to these dimensions
+
+
+def process_image(img_name):
+    img = Image.open(img_name)
+
+    img = img.filter(ImageFilter.GaussianBlur(15)) ## This is CPU bound
+
+    img.thumbnail(size)
+    img.save(f'processed/{img_name}') ## This is IO bound
+    print(f'{img_name} was processed...')
+
+
+with concurrent.futures.ProcessPoolExecutor() as executor:
+    executor.map(process_image, img_names)
+
+
+t2 = time.perf_counter()
+
+print(f'Finished in {t2-t1} seconds')
